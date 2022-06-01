d8c77de02f8538618e16521f8c27fc9ac1acd5b9
Rhodri Rees <2373655r@student.gla.ac.uk>
2022-06-01 22:43:12 +0100

    First Commit
    React boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="App-navbar">
+      <a 
+        className="App-link"
+        href="/#"
+      >
+        Overview</a>
+      <a 
+      className="App-link"
+      href="/#"
+      >
+        Projects
         </a>
-      </header>
+      </div>
+
+
+
+      { <header className="App-header">
+        <h1>Rhodri Rees</h1>
+        <h3>Computer Scientist</h3>
+      </header>}
+    
+
+      <div class='App-container'>
+        <div className="App-left">
+          <h2> About Me</h2>
+          <img className="img-fluid" 
+          src={`${process.env.PUBLIC_URL}/assets/5.png`} 
+          alt="Me"/>
+          <p>
+          Third year computing science student with a wide variety of experience 
+both academic and extracurricular now looking to gain real world skill. 
+Tenacious, creative, and self-motivated. Passionate about cyber security 
+and human-computer interaction. Strong problem solving skills with a 
+background of maths and physics.
+
+          </p>
+        </div>
+        <div className="App-right">
+          <h2> Experience </h2>
+          <h5> University of Glasgow,  UK 2018 – 2022 </h5>
+          <p> On track for First-Class Honours in Computing Science, BSc 
+          </p>
+          <p>
+            Information Security and HCI strand Maths and Physics minors in first and second year with average A
+          </p>
+          <h5> American Embassy School, India 2016-2018</h5> 
+          <p>Overall score of 39 in International Baccalaureate
+          </p>
+          <p>
+          Higher level: Physics, Computer Science, Maths 
+          </p>
+          <p>
+          Standard level: Spanish, English, Economics
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
